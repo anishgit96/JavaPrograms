20be2ec2b84bff90ccda8c3bf92aded70543972b
Anish Sethiya <anishsethiya22@gmail.com>
2021-05-12 20:24:21 +0530

    subString() Method Use Case

---
+++ String_subStringMethodUse.java
@@ -0,0 +1,9 @@
+public class String_subStringMethodUse {
+    public static void main(String[] args) {
+
+        String str = new String("Welcome to GIIT Solutions");
+
+        System.out.print("The extracted substring is : ");
+        System.out.println(str.substring(10, 16));
+    }
+}
