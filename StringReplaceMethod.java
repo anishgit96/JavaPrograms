1d46745df6d24b55f39e5691b9919595f37dc051
Anish Sethiya <anishsethiya22@gmail.com>
2021-05-13 20:57:15 +0530

    String replace() Use

---
+++ StringReplaceMethod.java
@@ -0,0 +1,7 @@
+public class StringReplaceMethod {
+
+    public static void main(String[] args) {
+        String str= "Go With The Flow";
+        String str2=str.replace('G',' ');
+        System.out.println(str2);
+    }}
